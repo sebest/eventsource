d5875e5df35bc5d68c8b0c608987d9e472f1b638
Lex Robinson <lex@playgen.com>
2014-12-01 09:40:47 +0000

    go test has its own (better) timeout mechanism. (--timeout=5)

--- server_test.go
+++ server_test.go
@@ -32,40 +32,20 @@ func TestServer(t *testing.T) {
 			var w testResponder
 			w.ResponseRecorder = httptest.NewRecorder()
 			Convey("closes active connections", func() {
-				c := make(chan bool)
-				completed := false
 				time.AfterFunc(time.Microsecond, func() {
 					srv.Close()
 					closed = true
 				})
-				time.AfterFunc(time.Millisecond*10, func() {
-					if !completed {
-						panic("Test has hung.")
-					}
-					c <- true
-				})
 				// This blocks while the channel is open
 				srv.Handler(channel)(w, r)
-				completed = true
-				woop := <-c
-				So(woop, ShouldBeTrue)
+				So(true, ShouldBeTrue)
 			})
 			Convey("denies new connections", func() {
-				c := make(chan bool)
-				completed := false
 				srv.Close()
 				closed = true
-				time.AfterFunc(time.Millisecond, func() {
-					if !completed {
-						panic("Test has hung.")
-					}
-					c <- true
-				})
 				// This shouldn't block
 				srv.Handler(channel)(w, r)
 				So(w.Code, ShouldEqual, http.StatusGone)
-				completed = true
-				<-c
 			})
 			Convey("panics when used", func() {
 				srv.Close()
